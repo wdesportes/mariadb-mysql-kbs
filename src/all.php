a8b0afa3d24776aa6fb4c687d855a2701fc002c8
William Desportes <williamdes@wdes.fr>
2018-06-06 22:19:26 +0200

    [MySQL] Added more refs
    - Added audit-log
    - Added replication-options
    - Replication option slave
    - Cluster options

---
+++ src/all.php
@@ -0,0 +1,26 @@
+<?php
+
+$files = glob(__DIR__."/../data/mariadb-*.json");
+
+$variables = array();
+
+$nbr = 0;
+foreach($files as $file) {
+    $data  = json_decode(file_get_contents($file));
+    if(isset($data->data) === false)
+        continue;
+    else
+        $data = $data->data;
+
+    foreach($data as $doc) {
+        if(in_array($doc->name,$variables) == false){
+            array_push($variables, $doc->name);
+        }
+    }
+    $nbr += count($data);
+}
+echo "NBR: ".$nbr."\r\n";
+echo "NBR_UNIQUE: ".count($variables)."\r\n";
+
+//file_put_contents(__DIR__."/../data/mariadb-names.json", json_encode($variables));
+//file_put_contents(__DIR__."/../data/mariadb-names.raw", implode(",", $variables));
