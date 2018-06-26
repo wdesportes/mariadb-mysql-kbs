0af36c4dc67f2cb0903999fa2351a5e46c9a0e78
William Desportes <williamdes@wdes.fr>
2018-06-26 03:11:36 +0200

    phpcs & prettier fixes +config

--- src/all.php
+++ src/all.php
@@ -1,19 +1,20 @@
 <?php
-
+declare(strict_types = 1);
 $files = glob(__DIR__."/../data/mariadb-*.json");
 
 $variables = array();
 
 $nbr = 0;
-foreach($files as $file) {
-    $data  = json_decode(file_get_contents($file));
-    if(isset($data->data) === false)
+foreach ($files as $file) {
+    $data = json_decode((string)file_get_contents($file));
+    if (isset($data->data) === false) {
         continue;
-    else
+    } else {
         $data = $data->data;
+    }
 
-    foreach($data as $doc) {
-        if(in_array($doc->name,$variables) == false){
+    foreach ($data as $doc) {
+        if (in_array($doc->name, $variables) == false) {
             array_push($variables, $doc->name);
         }
     }
